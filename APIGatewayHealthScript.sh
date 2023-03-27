dde348a77720431168c8be207336300b36c7d388
Jorra <jorra04@my.yorku.ca>
2023-03-26 22:55:49 -0400

    Added nginx and working health check for chids bidding server.

--- APIGatewayHealthScript.sh
+++ APIGatewayHealthScript.sh
@@ -6,6 +6,12 @@ echo "Cannot connect to the catalogue server, sleeping for 0.1s"
   sleep 0.1
 done
 
+
+while ! nc -z grpcServer 5003; do
+echo "Cannot connect to the auction server, sleeping for 0.1s"
+  sleep 0.1
+done
+
 # Start the api-gateway-server service (replace this with the correct command for starting your service)
 echo "Executing the APIGatewayServer"
 exec java -jar APIGatewayServer-0.0.1-SNAPSHOT.jar
