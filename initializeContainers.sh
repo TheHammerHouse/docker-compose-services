544575fc6306d9ac631643b9f2681f04d788d157
Jorra <jorra04@my.yorku.ca>
2023-03-29 20:52:07 -0400

    Added a script to compile docker images based on the target architecture.

--- initializeContainers.sh
+++ initializeContainers.sh
@@ -1,2 +1,23 @@
-docker-compose down
-docker-compose up -d --build --force-recreate
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+FILENAME=""
+
+if [ "$ARCH" == "x86_64" ]; then
+    FILENAME="docker-compose_intel.yml"
+elif [ "$ARCH" == "arm64" ]; then
+    FILENAME="docker-compose_arm.yml"
+else
+    echo "We are unable to build the containers for the following architecture: $ARCH"
+    exit 1
+fi
+
+if ! docker info > /dev/null 2>&1; then
+  echo "Docker is not running"
+  exit 1
+fi
+
+
+docker-compose -f $FILENAME down
+docker-compose -f $FILENAME up -d --build --force-recreate
